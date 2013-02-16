3ed789737d05934702f95af0b4db2f55dff3f57f
Lydia <llkats@gmail.com>
2013-02-16 15:35:11 -0800

    Took out the database stuff to start plain. Whee

--- app.js
+++ app.js
@@ -1,69 +1,81 @@
-      var MongoClient = require('mongodb').MongoClient,
-          Db = require('mongodb').Db,
-      Server = require('mongodb').Server,
-      Connec = require('mongodb').Connection,
+var http = require('http'),
+ express = require('express'),
+     app = express(),
+     pub = __dirname + '/public',
+  assert = require('assert');
 
-ReplSetServers = require('mongodb').ReplSetServers,
-      ObjectID = require('mongodb').ObjectID,
-        Binary = require('mongodb').Binary,
-     GridStore = require('mongodb').GridStore,
-          Code = require('mongodb').Code,
-          BSON = require('mongodb').pure().BSON,
 
-        http = require('http'),
-     express = require('express'),
-         app = express(),
-         pub = __dirname + '/public',
-      assert = require('assert');
-
-var host = 'localhost';
-var port = Connec.DEFAULT_PORT;
-var   db = new Db('tob-checklist', new Server("127.0.0.1", 27017,
-  {auto_reconnect: false, poolSize: 4}), {w:0, native_parser: false});
-// var mongoclient = new MongoClient(new Server("localhost", 27017, {native_parser: true}));
-
-// var ObjectID = db.bson_serializer.ObjectID;
-
-// app.use(app.router);
+app.use(app.router);
 app.use(express.bodyParser());
 app.use(express.static(pub));
 app.use(express.errorHandler());
-// app.use(express.cookieSession({'secret': 'your mom'}));
 
 app.set('views', __dirname + '/lib/views');
 app.set('view engine', 'jade');
 
 
-var list = {
-    "_id" : 1,
-  "books" : [{
+var list = [{
     "info" : "HHhH by Laurent Binet",
      "url" : "http://www.barnesandnoble.com/w/hhhh-laurent-binet/1110779344"
+  }, {
+    "info" : "The Round House by Louise Erdrich",
+     "url" : "http://www.barnesandnoble.com/w/the-round-house-louise-erdrich/1108939199?ean=9780062065261"
+  }, {
+    "info" : "Gone Girl by Gillian Flynn",
+     "url" : "http://www.barnesandnoble.com/w/gone-girl-gillian-flynn/1105608095?ean=9780307588388"
+  }, {
+    "info" : "The Fault in Our Stars by John Green",
+     "url" : "http://www.barnesandnoble.com/w/the-fault-in-our-stars-john-green/1104045488?ean=9781101569184&amp;itm=1&amp;usri=the+fault+in+our+stars"
+  }, {
+    "info" : "Arcadia by Lauren Groff",
+     "url" : "http://www.barnesandnoble.com/w/arcadia-lauren-groff/1101556584?ean=9781401342784&amp;itm=1&amp;usri=arcadia+"
+  }, {
+    "info" : "How Should a Person Be? by Sheila Heti",
+     "url" : "http://www.barnesandnoble.com/w/how-should-a-person-be-sheila-heti/1111872185?ean=9781429943482&amp;itm=1&amp;usri=how+should+a+person+be%3f"
+  }, {
+    "info" : "May We Be Forgiven by A.M. Homes",
+     "url" : "http://www.barnesandnoble.com/w/may-we-be-forgiven-am-homes/1108892752?ean=9781101601143&amp;itm=1&amp;usri=may+we+be+forgiven+"
+  }, {
+    "info" : "The Orphan Master's Son by Adam Johnson",
+     "url" : "http://www.barnesandnoble.com/w/orphan-masters-son-adam-johnson/1100054498?ean=9780679643999&amp;itm=1&amp;usri=the+orphan+master%E2%80%99s+son+"
+  }, {
+    "info" : "Ivyland by Miles Klee",
+     "url" : "http://www.barnesandnoble.com/w/ivyland-miles-klee/1108355924?ean=2940014773027&amp;itm=1&amp;usri=ivyland+"
+  }, {
+    "info" : "Bring Up the Bodies by Hilary Mantel",
+     "url" : "http://www.barnesandnoble.com/w/bring-up-the-bodies-hilary-mantel/1108185320?ean=9781429947657&amp;itm=1&amp;usri=bring+up+the+bodies+"
+  }, {
+    "info" : "The Song of Achilles by Madeline Miller",
+     "url" : "http://www.barnesandnoble.com/w/the-song-of-achilles-madeline-miller/1103167914?ean=9780062060631&amp;itm=1&amp;usri=the+song+of+achilles+"
+  }, {
+    "info" : "Dear Life by Alice Munro",
+     "url" : "http://www.barnesandnoble.com/w/dear-life-alice-munro/1108939524?ean=9780307961044&amp;itm=1&amp;usri=dear+life+"
+  }, {
+    "info" : "Where'd You Go Bernadette by Maria Semple",
+     "url" : "http://www.barnesandnoble.com/w/whered-you-go-bernadette-maria-semple/1106244241?ean=9780316204286&amp;itm=1&amp;usri=where%E2%80%99d+you+go+bernadette+"
+  }, {
+    "info" : "Beautiful Ruins by Jess Walter",
+     "url" : "http://www.barnesandnoble.com/w/the-beautiful-ruins-jess-walter/1103427003?ean=9780062098085&amp;itm=1&amp;usri=beautiful+ruins+"
+  }, {
+    "info" : "Building Stories by Chris Ware",
+     "url" : "http://www.barnesandnoble.com/w/building-stories-chris-ware/1109722191?ean=9780375424335"
+  }, {
+    "info" : "Fobbit by David Abrams",
+     "url" : "http://www.barnesandnoble.com/w/fobbit-david-abrams/1109170055?ean=9780802194084&amp;itm=1&amp;usri=fobbit+"
+  }, {
+    "info" : "Billy Lynn's Long Halftime Walk by Ben Fountain",
+     "url" : "http://www.barnesandnoble.com/w/billy-lynns-long-halftime-walk-ben-fountain/1106580295?ean=9780062096821&amp;itm=1&amp;usri=billy+lynn%E2%80%99s+long+halftime+walk+"
+  }, {
+    "info" : "The Yellow Birds by Kevin Powers",
+     "url" : "http://www.barnesandnoble.com/w/the-yellow-birds-kevin-powers/1108616399?ean=9780316219358&amp;itm=1&amp;usri=the+yellow+birds+"
   }
-]};
+];
+
 
 var simple = { "hi" : "one"};
 
-db.open(function(err, db) {
-  db = db.db('tob-checklist');
-  db.collection('test').insert(list, {w:1}, function(err, result) {});
-  // db.collection('test').find({_id: 1}, function(err, cursor) {
-  //   cursor.toArray(function(err, items) {
-  //     db.close();
-  //   });
-  // });
-});
-
 
 app.get('/', function(req, res) {
-  var list;
-
-  // mongoclient.open(function(err, db) {
-  //   db = mongoclient.db('tob-checklist-tests');
-  //   list = db.collection('book-list');
-
-  //   // mongoclient.close();
-  // });
 
   res.render('index', {list: list});
   // res.send(list);
